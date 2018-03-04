970827ba79b954c225661637235e66cbb9acac8a
RaJiska <dodo.lasticot@gmail.com>
2018-03-04 18:35:00 +0100

    Tests Norm Fix

--- tests/src/test/test_strstr.c
+++ tests/src/test/test_strstr.c
@@ -7,22 +7,27 @@
 
 #include "tests.h"
 
+static void test_initial(char *buffer)
+{
+	strcpy(buffer, "abcd");
+	cr_assert_eq(fncs.my_strstr(buffer, "c"), buffer + 2);
+	cr_assert_eq(fncs.my_strstr(buffer, "bc"), buffer + 1);
+	cr_assert_eq(fncs.my_strstr(buffer, "d"), buffer + 3);
+	cr_assert_eq(fncs.my_strstr(buffer, "cd"), buffer + 2);
+	cr_assert_eq(fncs.my_strstr(buffer, "abc"), buffer);
+	cr_assert_eq(fncs.my_strstr(buffer, "abcd"), buffer);
+	cr_assert_eq(fncs.my_strstr(buffer, "abcde"), NULL);
+	cr_assert_eq(fncs.my_strstr(buffer, "de"), NULL);
+	cr_assert_eq(fncs.my_strstr(buffer, ""), buffer);
+}
+
 Test(libasm, strstr, .init = setup)
 {
 	char buffer[100] = { 1 };
 
 	cr_assert_eq(fncs.my_strstr("abcd", "z"), NULL);
 	cr_assert_eq(fncs.my_strstr("abcd", "abx"), NULL);
-	strcpy(&buffer[0], "abcd");
-	cr_assert_eq(fncs.my_strstr(&buffer[0], "c"), &buffer[2]);
-	cr_assert_eq(fncs.my_strstr(&buffer[0], "bc"), &buffer[1]);
-	cr_assert_eq(fncs.my_strstr(&buffer[0], "d"), &buffer[3]);
-	cr_assert_eq(fncs.my_strstr(&buffer[0], "cd"), &buffer[2]);
-	cr_assert_eq(fncs.my_strstr(&buffer[0], "abc"), &buffer[0]);
-	cr_assert_eq(fncs.my_strstr(&buffer[0], "abcd"), &buffer[0]);
-	cr_assert_eq(fncs.my_strstr(&buffer[0], "abcde"), NULL);
-	cr_assert_eq(fncs.my_strstr(&buffer[0], "de"), NULL);
-	cr_assert_eq(fncs.my_strstr(&buffer[0], ""), &buffer[0]);
+	test_initial(&buffer[0]);
 	strcpy(&buffer[0], "ababa");
 	cr_assert_eq(fncs.my_strstr(&buffer[0], "ba"), &buffer[1]);
 	strcpy(&buffer[0], "");
