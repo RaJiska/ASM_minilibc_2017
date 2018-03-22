f564139900ea81daea6fc2ce0675831c49bc19d5
RaJiska <dodo.lasticot@gmail.com>
2018-03-22 10:02:09 +0100

    x64 Read/Write now set errno

--- minilibc_x64/src/read.asm
+++ minilibc_x64/src/read.asm
@@ -1,5 +1,7 @@
 	bits 64
 	global read:function
+	extern __errno_location
+	;extern errno
 
 	section .text:
 	; ssize_t read(int fd, void *buf, size_t count)
@@ -10,4 +12,17 @@ read:
 
 	mov rax, 0x0
 	syscall
+
+	cmp rax, 0
+	jge short .return
+	cmp rax, -4096
+	jl short .return
+	mov rdx, rax
+	neg DWORD edx
+	call __errno_location WRT ..plt
+	mov DWORD [rax], edx
+	mov rax, -1
+
+	.return:
+
 	ret
--- minilibc_x64/src/write.asm
+++ minilibc_x64/src/write.asm
@@ -1,5 +1,6 @@
 	bits 64
 	global write:function
+	extern __errno_location
 
 	section .text:
 	; ssize_t write(int fd, const void *buf, size_t count)
@@ -10,4 +11,17 @@ write:
 
 	mov rax, 0x1
 	syscall
+
+	cmp rax, 0
+	jge short .return
+	cmp rax, -4096
+	jl short .return
+	mov rdx, rax
+	neg DWORD edx
+	call __errno_location WRT ..plt
+	mov DWORD [rax], edx
+	mov rax, -1
+
+	.return:
+
 	ret
